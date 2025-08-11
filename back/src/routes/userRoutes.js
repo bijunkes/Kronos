0a6aa1c55811542d1b8f5b5e7f7ecc46e49b6834
Bianca <bianca.jr2007@gmail.com>
2025-08-11 10:03:02 -0300

    Importações
    Alteração nas importações

--- back/src/routes/userRoutes.js
+++ back/src/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express');
+import express from 'express';
+import verificarToken from '../middlewares/userMiddleware.js';
+import {cadastro, login, perfil} from '../controllers/userController.js';
+
 const router = express.Router();
-import verificarToken from '../middlewares/userMiddleare.js';
-import {cadastro, login, perfil} from '../src/controllers/userControllers.js';
 
 router.post('/cadastro', cadastro);
 router.post('/login', login);
