82d55d9681a6e7f7772114796b59064dda273169
Your Name <you@example.com>
2025-09-15 12:04:45 -0300

    Termino da página Kanban e protótipo de funcionamento

--- front/src/containers/Kanban/index.jsx
+++ front/src/containers/Kanban/index.jsx
@@ -1,25 +1,120 @@
 import {React, useState} from 'react';
-import {Container,Painel, BoxTitulo, BoxTarefas, NomeTarefa, Icones} from './style.js';
+import {Container,Painel, BoxTitulo, BoxTarefas, NomeTarefa, Icones, BoxAdicionar} from './style.js';
 
 
 function Kanban() {
      
+    const [tarefas, setTarefas] = useState([
+        {id: 'tarefa1', nome: 'Tarefa 1', status: 1, icons: 'icones1'}
+    ])
     
-    const deletar = () => {
+                    
+    const deletar = (tarefaId) => {
 
-        console.log("Deletando")
+        setTarefas((prevTarefas) => prevTarefas.filter(tarefa => tarefa.id !== tarefaId));
         
     }
+    const proximo = (tarefaId) => {
+
+        setTarefas((prevTarefas) => 
+            prevTarefas.map(tarefa => 
+                tarefa.id === tarefaId 
+                    ? { 
+                        ...tarefa, 
+                        status: tarefa.status < 3 ? tarefa.status + 1 : 3, 
+                        icons: tarefa.status === 2 ? 'icones3' : 'icones2' 
+                        
+                    } 
+                    : tarefa,
+                    console.log("teste")
+            )
+        );
+
+    }
+    const anterior = (tarefaId) => {
+
+        setTarefas((prevTarefas) => 
+            prevTarefas.map(tarefa => 
+                tarefa.id === tarefaId 
+                    ? { 
+                        ...tarefa, 
+                        status: tarefa.status > 1 ? tarefa.status - 1 : 1, 
+                        icons: tarefa.status === 2 ? 'icones1' :'icones2' 
+                        
+                    } 
+                    : tarefa,
+                    console.log("teste")
+            )
+        );
+
+    }
+    const renderIcons = (iconsType, tarefaId) => {
+        if (iconsType === 'icones1') {
+            return (
+                <>
+                    <Icones className="material-symbols-outlined" onClick={()=> deletar(tarefaId)}>delete</Icones>
+                    <Icones className="material-symbols-outlined" onClick={() => proximo(tarefaId)}>arrow_forward</Icones>
+                </>
+            );
+        } else if (iconsType === 'icones2') {
+            return (
+                <>
+                    <Icones className="material-symbols-outlined" onClick={()=> deletar(tarefaId)}>delete</Icones>
+                    <Icones  className="material-symbols-outlined" onClick={()=> anterior(tarefaId)}>arrow_left_alt</Icones>
+                    <Icones className="material-symbols-outlined" onClick={() => proximo(tarefaId)}>arrow_forward</Icones>
+                </>
+            );
+        }else if (iconsType === 'icones3') {
+            return (
+                <>
+                    <Icones className="material-symbols-outlined" onClick={()=> deletar(tarefaId)}>delete</Icones>
+                    <Icones  className="material-symbols-outlined" onClick={()=> anterior(tarefaId)}>arrow_left_alt</Icones>
+                </>
+            );
+        }
+        return null;
+    };
+    const adicionarTarefa = (campo) => {
+        const novaTarefa = {
+            id: `tarefa${tarefas.length + 1}`,
+            nome: `Tarefa ${tarefas.length + 1}`,
+            status: campo.parentNode.id, 
+            icons: 'icones' + campo.parentNode.id
+        };
+        console.log("Adicionando nova tarefa:", novaTarefa);
+        setTarefas([...tarefas, novaTarefa]);
+    };
+
     return(
         <>
             <Container>
-                <Painel id='AFazer'>
+                <Painel id='1'>
                     <BoxTitulo>A Fazer</BoxTitulo>
-                    <BoxTarefas><NomeTarefa>Tarefa 1</NomeTarefa>
-                        <Icones className="material-symbols-outlined" onClick={deletar}>delete</Icones>
-                    </BoxTarefas></Painel>
-                <Painel id='Fazendo'><BoxTitulo>Fazendo</BoxTitulo></Painel>
-                <Painel id='Feito'><BoxTitulo>Feito</BoxTitulo></Painel>
+                    {tarefas.filter(tarefa => tarefa.status == 1).map(tarefa => (
+                        <BoxTarefas key={tarefa.id} id={tarefa.id}>
+                            <NomeTarefa>{tarefa.nome}</NomeTarefa>
+                            {renderIcons(tarefa.icons, tarefa.id)}
+                        </BoxTarefas>
+                    ))}
+                    <BoxAdicionar onClick={(e) => {adicionarTarefa(e.target)}}>Adicionar Tarefa</BoxAdicionar></Painel>
+                <Painel id='2'><BoxTitulo>Fazendo</BoxTitulo>
+                {tarefas.filter(tarefa => tarefa.status == 2).map(tarefa => (
+                        <BoxTarefas key={tarefa.id} id={tarefa.id}>
+                            <NomeTarefa>{tarefa.nome}</NomeTarefa>
+                            {renderIcons(tarefa.icons, tarefa.id)}
+                        </BoxTarefas>
+                    ))}
+                    <BoxAdicionar onClick={(e) => {adicionarTarefa(e.target)}}>Adicionar Tarefa</BoxAdicionar>
+                </Painel>
+                <Painel id='3'><BoxTitulo>Feito</BoxTitulo>
+                {tarefas.filter(tarefa => tarefa.status == 3).map(tarefa => (
+                        <BoxTarefas key={tarefa.id} id={tarefa.id}>
+                            <NomeTarefa>{tarefa.nome}</NomeTarefa>
+                            {renderIcons(tarefa.icons, tarefa.id)}
+                        </BoxTarefas>
+                    ))}
+                    <BoxAdicionar onClick={(e) => {adicionarTarefa(e.target)}}>Adicionar Tarefa</BoxAdicionar>
+                </Painel>
             </Container>
             
         </>
